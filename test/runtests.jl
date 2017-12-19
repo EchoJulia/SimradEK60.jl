e996908c130e56dab071218eaba32cbde54df810
Rob Blackwell <rob@robblackwell.com>
2017-12-19 11:49:20 +0000

    Tests for all frequencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,11 +28,23 @@ _Sv = Sv(Pr, lambda, G, psi, cv, alpha, pt, tau, Sac,
 ps =collect(pings(SimradRaw.EK60_SAMPLE));
 @test length(ps) == 1092
 
-# 38kHz pings
+# 38 kHz pings
 
 ps38 = [p for p in ps if p.frequency == 38000];
 
 @test length(ps38) == 273
+
+# 70 kHz pings
+
+ps70 = [p for p in ps if p.frequency == 70000];
+
+@test length(ps70) == 273
+
+# 120 kHz pings
+
+ps120 = [p for p in ps if p.frequency == 120000];
+
+@test length(ps120) == 273
 
 # 200 kHz pings
 
@@ -40,7 +52,7 @@ ps200 = [p for p in ps if p.frequency == 200000];
 
 @test length(ps200) == 273
 
-# 38kHz volume backscatter
+# 38 kHz volume backscatter
 
 Sv38 = Sv(ps38);
 
@@ -50,7 +62,7 @@ m, n = size(Sv38)
 
 @test typeof(Sv38[1,1]) == Float32
 
-# 38 kHz power
+# Power
 
 p38 = power(ps38)
 m, n = size(p38)
@@ -65,8 +77,14 @@ m, n = size(pdb38)
 
 @test typeof(pdb38[1,1]) == Float32
 
-# 200kHz volume backscatter
+pdb70 = powerdb(ps70)
+pdb120 = powerdb(ps120)
+pdb200 = powerdb(ps200)
 
+# Volume backscatter
+
+Sv70 = Sv(ps70)
+Sv120 = Sv(ps120)
 Sv200 = Sv(ps200)
 
 m, n = size(Sv200)
@@ -76,6 +94,9 @@ m, n = size(Sv200)
 # Athwartships
 
 at38 = athwartshipangle(ps38);
+at70 = athwartshipangle(ps70);
+at120 = athwartshipangle(ps120);
+at200 = athwartshipangle(ps200);
 
 m, n = size(at38)
 @test m == 5934
@@ -84,6 +105,9 @@ m, n = size(at38)
 # Alongships
 
 al38 = alongshipangle(ps38);
+al70 = alongshipangle(ps70);
+al120 = alongshipangle(ps120);
+al200 = alongshipangle(ps200);
 
 m, n = size(al38)
 @test m == 5934
@@ -110,12 +134,30 @@ filename = joinpath(dirname(@__FILE__),
 data = matread(filename)["data"]
 
 @test al38 == data["pings"]["alongship_e"][:,1][1]
-@test at38 == data["pings"]["athwartship_e"][:,1][1]
-@test isapprox(data["pings"]["power"][:,1][1], pdb38, atol = 0.000001)
+@test al70 == data["pings"]["alongship_e"][:,2][1]
+@test al120 == data["pings"]["alongship_e"][:,3][1]
+@test al200 == data["pings"]["alongship_e"][:,4][1]
 
+@test at38 == data["pings"]["athwartship_e"][:,1][1]
+@test at70 == data["pings"]["athwartship_e"][:,2][1]
+@test at120 == data["pings"]["athwartship_e"][:,3][1]
+@test at200 == data["pings"]["athwartship_e"][:,4][1]
+
+@test isapprox(data["pings"]["power"][:,1][1], pdb38, atol = 0.000001)
+@test isapprox(data["pings"]["power"][:,2][1], pdb70, atol = 0.000001)
+@test isapprox(data["pings"]["power"][:,3][1], pdb120, atol = 0.000001)
+@test isapprox(data["pings"]["power"][:,4][1], pdb200, atol = 0.000001)
 
 @test ps38[1].absorptioncoefficient ==  data["pings"]["absorptioncoefficient"][1][1]
+@test ps70[1].absorptioncoefficient ==  data["pings"]["absorptioncoefficient"][2][1]
+@test ps120[1].absorptioncoefficient ==  data["pings"]["absorptioncoefficient"][3][1]
+@test ps200[1].absorptioncoefficient ==  data["pings"]["absorptioncoefficient"][4][1]
+
 @test ps38[1].transmitpower ==  data["pings"]["transmitpower"][1][1]
+@test ps70[1].transmitpower ==  data["pings"]["transmitpower"][2][1]
+@test ps120[1].transmitpower ==  data["pings"]["transmitpower"][3][1]
+@test ps200[1].transmitpower ==  data["pings"]["transmitpower"][4][1]
+
 @test ps38[1].soundvelocity ==  data["pings"]["soundvelocity"][1][1]
 @test ps38[1].pulselength ==  data["pings"]["pulselength"][1][1]
 @test ps38[1].sampleinterval ==  data["pings"]["sampleinterval"][1][1]
@@ -126,13 +168,27 @@ data = matread(filename)["data"]
 # TODO Check that this test would fail
 @test isapprox(R(ps38[1])[3:end], data["pings"]["range"][1][1:end-2], atol = 0.000001)
 
-
 @test ps38[1].gain ==  data["config"]["gain"][1]
-@test ps38[1].frequency ==  data["config"]["frequency"][1]
-@test ps38[1].equivalentbeamangle ==  data["config"]["equivalentbeamangle"][1]
-@test isapprox(ps38[1].sacorrectiontable,  data["config"]["sacorrectiontable"][1], atol = 0.000001)
-@test isapprox(ps38[1].pulselengthtable,  data["config"]["pulselengthtable"][1], atol = 0.000001)
+@test ps70[1].gain ==  data["config"]["gain"][2]
+@test ps120[1].gain ==  data["config"]["gain"][3]
+@test ps200[1].gain ==  data["config"]["gain"][4]
 
+@test ps38[1].frequency ==  data["config"]["frequency"][1]
+@test ps70[1].frequency ==  data["config"]["frequency"][2]
+@test ps120[1].frequency ==  data["config"]["frequency"][3]
+@test ps200[1].frequency ==  data["config"]["frequency"][4]
+
+@test ps38[1].equivalentbeamangle ==  data["config"]["equivalentbeamangle"][1]
+
+@test isapprox(ps38[1].sacorrectiontable,  data["config"]["sacorrectiontable"][1], atol = 0.000001)
+@test isapprox(ps70[1].sacorrectiontable,  data["config"]["sacorrectiontable"][2], atol = 0.000001)
+@test isapprox(ps120[1].sacorrectiontable,  data["config"]["sacorrectiontable"][3], atol = 0.000001)
+@test isapprox(ps200[1].sacorrectiontable,  data["config"]["sacorrectiontable"][4], atol = 0.000001)
+
+@test isapprox(ps38[1].pulselengthtable,  data["config"]["pulselengthtable"][1], atol = 0.000001)
+@test isapprox(ps70[1].pulselengthtable,  data["config"]["pulselengthtable"][2], atol = 0.000001)
+@test isapprox(ps120[1].pulselengthtable,  data["config"]["pulselengthtable"][3], atol = 0.000001)
+@test isapprox(ps200[1].pulselengthtable,  data["config"]["pulselengthtable"][4], atol = 0.000001)
 
 # We are very close to ReadEKRaw
 
@@ -141,6 +197,14 @@ sv = data["pings"]["Sv"][:,1][1]
 # println(sv[1:10,1:10])
 @test isapprox(Sv38, sv)
 
+sv = data["pings"]["Sv"][:,2][1]
+@test isapprox(Sv70, sv)
+
+sv = data["pings"]["Sv"][:,3][1]
+@test isapprox(Sv120, sv)
+
+sv = data["pings"]["Sv"][:,4][1]
+@test isapprox(Sv200, sv)
 
 # Comparison with EchoView
 
